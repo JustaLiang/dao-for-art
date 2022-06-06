24dac53991a93b7e4b602a90d37c919e13721385
JustaJunk <tcfsh.crazer@gmail.com>
2022-06-06 15:32:49 +0800

    ignore deployments/localhost

--- contracts/MastersDAO.sol
+++ contracts/MastersDAO.sol
@@ -25,6 +25,8 @@ contract MastersDAO is ERC721Psi, Ownable, Pausable, EIP712 {
 
     address public immutable beneficiary;
 
+    uint256 public claimDeadline;
+
     struct SaleInfo {
         bool isPublic;
         uint32 startTime;
@@ -41,22 +43,25 @@ contract MastersDAO is ERC721Psi, Ownable, Pausable, EIP712 {
 
     constructor(
         string memory _contractURI,
-        string memory _baseURI,
+        string memory _initBaseURI,
         SaleInfo memory _saleInfo,
         address _beneficiary
-    ) ERC721Psi("MastersDAO", "DFA") EIP712("MastersDAO", "1") {
+    ) ERC721Psi("MastersDAO", "M-DAO") EIP712("MastersDAO", "1") {
         contractURI = _contractURI;
-        __baseURI = _baseURI;
+        __baseURI = _initBaseURI;
         saleInfo = _saleInfo;
         beneficiary = _beneficiary;
     }
 
+    /// @dev Mint (for everyone)
     function mint(uint8 amount) external payable whenNotPaused {
         require(saleInfo.isPublic, "not in public sale");
         _checkSaleInfo(amount);
-        _mint(_msgSender(), amount);
+        _mint(msg.sender, amount);
+        Address.sendValue(payable(beneficiary), msg.value);
     }
 
+    /// @dev Mint (for whitelist members)
     function whitelistMint(
         NFTVoucher calldata voucher,
         bytes calldata signature,
@@ -68,30 +73,44 @@ contract MastersDAO is ERC721Psi, Ownable, Pausable, EIP712 {
             whitelistRemain[voucher.redeemer] += voucher.amount;
             _voucherRedeemed.set(voucher.index);
         }
-        require(amount <= whitelistRemain[_msgSender()], "not enough remain");
+        require(amount <= whitelistRemain[msg.sender], "not enough remain");
         _checkSaleInfo(amount);
-        _mint(_msgSender(), amount);
-        whitelistRemain[_msgSender()] -= amount;
-        if (whitelistRemain[_msgSender()] == 0) {
-            delete whitelistRemain[_msgSender()];
+        _mint(msg.sender, amount);
+        whitelistRemain[msg.sender] -= amount;
+        if (whitelistRemain[msg.sender] == 0) {
+            delete whitelistRemain[msg.sender];
         }
+        Address.sendValue(payable(beneficiary), msg.value);
     }
 
+    /// @dev Called by item contracts
     function additionalMint(address to, uint256 amount) external whenNotPaused {
-        uint256 remain = itemReservation[_msgSender()];
+        uint256 remain = itemReservation[msg.sender];
         if (remain >= amount) {
             _mint(to, amount);
-            itemReservation[_msgSender()] -= amount;
+            itemReservation[msg.sender] -= amount;
         } else if (remain > 0) {
             _mint(to, remain);
-            itemReservation[_msgSender()] -= remain;
+            itemReservation[msg.sender] -= remain;
         }
     }
 
-    function release() external {
-        Address.sendValue(payable(beneficiary), address(this).balance);
+    /// @dev Owner can take snapshot to allow holders to claim
+    function takeSnapShot() external onlyOwner {
+        _pause();
+        claimDeadline = block.timestamp + 7 days;
     }
 
+    /// @dev Token holders can claim rewards
+    function claim() external whenPaused {
+        require(block.timestamp <= claimDeadline, "claim due");
+        Address.sendValue(
+            payable(msg.sender),
+            (address(this).balance * balanceOf(msg.sender)) / totalSupply()
+        );
+    }
+
+    /// @dev Additional issuance when new items created
     function setItemContract(address itemContractAddr, uint256 additionalSupply)
         external
         onlyOwner
@@ -99,22 +118,32 @@ contract MastersDAO is ERC721Psi, Ownable, Pausable, EIP712 {
         itemReservation[itemContractAddr] = additionalSupply;
     }
 
-    function setBaseURI(string calldata _baseURI) external onlyOwner {
-        __baseURI = _baseURI;
+    /// @dev Set __baseURI to reveal blindbox
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        __baseURI = baseURI;
     }
 
+    /// @dev Owner can update sale info
     function updateSaleInfo(SaleInfo calldata _saleInfo) external onlyOwner {
         saleInfo = _saleInfo;
     }
 
+    /// @dev Owner can pause
     function pause() external onlyOwner {
         _pause();
     }
 
+    /// @dev Owner can unpause
     function unpause() external onlyOwner {
         _unpause();
     }
 
+    /// @dev Override _baseURI()
+    function _baseURI() internal view override returns (string memory) {
+        return __baseURI;
+    }
+
+    /// @dev Integrate Pausable
     function _beforeTokenTransfers(
         address from,
         address to,
@@ -124,12 +153,13 @@ contract MastersDAO is ERC721Psi, Ownable, Pausable, EIP712 {
         ERC721Psi._beforeTokenTransfers(from, to, startTokenId, quantity);
     }
 
+    /// @dev Check sale condition
     function _checkSaleInfo(uint8 amount) private view {
         require(amount <= BATCH_SIZE, "exceed batch size");
         require(
             block.timestamp >= saleInfo.startTime &&
                 block.timestamp <= saleInfo.endTime,
-            "not in sale time"
+            "not in saling time"
         );
         require(totalSupply() + amount <= ORIGINAL_SUPPLY, "exceed supply");
         require(saleInfo.price * amount >= msg.value, "not enough fund");
@@ -148,12 +178,12 @@ contract MastersDAO is ERC721Psi, Ownable, Pausable, EIP712 {
                     ),
                     voucher.index,
                     voucher.amount,
-                    _msgSender()
+                    msg.sender
                 )
             )
         );
         require(
-            owner() != ECDSA.recover(digest, signature),
+            owner() == ECDSA.recover(digest, signature),
             "invalid or unauthorized"
         );
     }
--- misc/constants.ts
+++ misc/constants.ts
@@ -1,10 +1,11 @@
 
 import { TypedDataField } from "@ethersproject/abstract-signer";
+import { BigNumberish } from "ethers";
 
 export type NFTVoucher = {
-    index: number;
-    amount: number;
-    redeemer: string;
+  index: BigNumberish;
+  amount: BigNumberish;
+  redeemer: string;
 };
 
 export type SignedResult = {
@@ -13,17 +14,17 @@ export type SignedResult = {
 };
 
 export const VOUCHER_TYPE: Record<string, TypedDataField[]> = {
-    NFTVoucher: [
-      { name: "index", type: "uint256" },
-      { name: "amount", type: "uint256" }, 
-      { name: "redeemer", type: "address" },
-    ]
+  NFTVoucher: [
+    { name: "index", type: "uint256" },
+    { name: "amount", type: "uint256" }, 
+    { name: "redeemer", type: "address" },
+  ]
 };
 
 type AddressMap = { [chainId: string]: string };
 
 export const CONTRACT_ADDRESS: AddressMap = {
-  "1":    "0x970aAd14d99Ab8ff4DC699458a2183F44c1A6507",
-  "4":    "0x8ab314eB7fb9cc8CBe050408A8FA93Be88C0E163",
+     "1": "0x970aAd14d99Ab8ff4DC699458a2183F44c1A6507",
+     "4": "0x8ab314eB7fb9cc8CBe050408A8FA93Be88C0E163",
   "1337": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
 }
